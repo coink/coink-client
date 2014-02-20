438a85a8ee331f4f79111c5658cc9cff2af5c2b6
Chris LaRose <cjlarose@email.arizona.edu>
2014-02-20 03:23:25 +0000

    Fix wallets collection parsing to match mysterious new api format

--- resources/public/js/collections/wallets.js
+++ resources/public/js/collections/wallets.js
@@ -3,6 +3,9 @@ define(['backbone', 'models/wallet'], function(Backbone, Wallet) {
     var Wallets = Backbone.Collection.extend({
         model: Wallet,
         urlRoot: "http://private-d789-coink.apiary.io/v1/wallets",
+        parse: function(response) {
+            return response.wallets;
+        },
         url: function() {
             return this.urlRoot;
         }
