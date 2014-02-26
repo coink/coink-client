abc1e31d37a192b337a4dc52c78d629eb3b340d3
Roey Chasman <rchasman@email.arizona.edu>
2014-02-25 18:49:13 -0700

    Validating login form with easy notifications via the new notification component :)

--- resources/public/js/components/login.js
+++ resources/public/js/components/login.js
@@ -1,23 +1,42 @@
-define(['react', 'jquery', 'router', 'models/profile'], function(React, $, router, profile) {
+define(['react', 'jquery', 'router', 'models/profile', 'utils'],
+function(React, $, router, profile, Utils) {
 
     var Login = React.createClass({
         handleSubmit: function(e) {
             e.preventDefault();
 
+            var username = this.refs.username.getDOMNode().value.trim();
+            var password = this.refs.password.getDOMNode().value.trim();
+
             var payload = {
-                "username" : this.refs.username.getDOMNode().value.trim(),
-                "password" : this.refs.password.getDOMNode().value.trim()
+                "username" : username,
+                "password" : password
             };
 
-            $.post(router.url_root + "/v1/session", JSON.stringify(payload),
-                function(data, textStatus, jqXHR) {
-                    profile.setToken(data.data.token, data.data.expires);
-                    router.navigate('wallets', {trigger: true});
-                }).fail(
-                function() {
-                    console.log("login-error");
-                    router.navigate('login', {trigger: true});
-                });
+            if (this.validateLogin(username, password)){
+                $.post(router.url_root + "/v1/session", JSON.stringify(payload),
+                    function(data, textStatus, jqXHR) {
+                        profile.setToken(data.data.token, data.data.expires);
+                        Utils.clearNotification();
+                        router.navigate('wallets', {trigger: true});
+                    }).fail(
+                    function() {
+                        Utils.updateNotification("error: login-error");
+                        router.navigate('login', {trigger: true});
+                    });
+            }
+        },
+
+        validateLogin: function(username, password) {
+            if ((username + password).length == 0)
+              Utils.updateNotification("error: please enter your email address and password");
+            else if (username.length == 0)
+              Utils.updateNotification("error: please enter your email address");
+            else if (password.length == 0)
+              Utils.updateNotification("error: please enter your password");
+            else
+                return true;
+            return false;
         },
 
         render: function() {
