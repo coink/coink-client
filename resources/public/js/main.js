dc3c915fbab02ce7961f4f830f1c2a8f10207721
Roey Chasman <rchasman@email.arizona.edu>
2014-03-23 14:14:47 -0700

    removed unused requirements, updated react to 0.10.0

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -5,10 +5,7 @@ require.config({
         'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery',
         'backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min',
         'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.3/underscore-min',
-        'react': '//cdnjs.cloudflare.com/ajax/libs/react/0.9.0/react',
-        'fastclick': '/js/scripts/fastclick',
-        'react.backbone': '/js/scripts/react.backbone',
-        'modernizr': '/js/scripts/modernizr-latest'
+        'react': '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react',
 
     },
     shim: {
@@ -24,12 +21,8 @@ require.config({
 
 require(['jquery', 'backbone', 'react', 'components/application', 'router', 'models/profile'], function($, Backbone, React, Application, router, profile) {
 
-    $(document).ready(function() {
-        var app = Application();
-        React.renderComponent(app, document.getElementById('coink'));
-    });
 
-
+    // Auth setup, if not doesn't set request header token if not logged in
     $.ajaxSetup({
         beforeSend: function (request) {
             if(profile.getToken() != null) {
@@ -38,13 +31,22 @@ require(['jquery', 'backbone', 'react', 'components/application', 'router', 'mod
         }
     });
 
+    // Begin rendering the webapp with the main react component 'Application'
+    $(document).ready(function() {
+        var app = Application();
+        React.renderComponent(app, document.getElementById('coink'));
+    });
+
+    // Default route if logged in
     if (profile.getToken() != null)
         router.setDefaultRoute('wallets');
     else
         router.setDefaultRoute("login");
 
+    // Single page webapp routing made nice for browser navigation
     Backbone.history.start({
         pushState: false, // False until compojure routing is figured out
         root: "/"
     });
+
 });
