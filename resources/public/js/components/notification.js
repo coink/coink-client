e0123477d16ee175fca0529866ff0cb8f94c33e4
Roey Chasman <rchasman@email.arizona.edu>
2014-03-23 20:52:41 -0700

    Added a close button on the notification box, proper foundation class attributes

--- resources/public/js/components/notification.js
+++ resources/public/js/components/notification.js
@@ -3,15 +3,20 @@ define(['react', 'models/notification'], function(React, notification) {
     var Notification = React.createClass({
         displayName: 'Notification',
         getInitialState: function() {
-            return {model : notification};
+            return {model: notification};
+        },
+
+        handleClose: function() {
+            this.state.model.clearNotification();
         },
 
         render: function() {
             if(this.state.model.get('message')) {
-                return React.DOM.div({id : 'notification-' + this.state.model.get('type')},
-                    React.DOM.h1({}, this.state.model.get('message')));
+                return React.DOM.div({"data-alert": '', id: 'notification', className: 'alert-box ' + this.state.model.get('type')},
+                    this.state.model.get('message'),
+                    React.DOM.a({href: '#', onClick: this.handleClose, className: 'close'}, 'x'));
             } else {
-                return React.DOM.div({id : 'notification'});
+                return React.DOM.div({id: 'notification'});
             }
         },
 
