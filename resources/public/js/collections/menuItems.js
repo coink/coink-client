d601170d06ea64bf2aea7430b30c8ba284af4415
Roey Chasman <rchasman@email.arizona.edu>
2014-03-30 14:57:59 -0700

    Refactored sidebar

---
+++ resources/public/js/collections/menuItems.js
@@ -0,0 +1,46 @@
+define(['backbone', 'models/menuItem'], function(Backbone, MenuItem) {
+
+    var MenuItems = Backbone.Collection.extend({
+        model: MenuItem,
+        initialize: function() {
+            this.set(menuItems);
+        }
+    });
+
+    var menuItems = [
+        {
+            text: 'Wallets',
+            route: ['wallets'],
+            icon: 'wallet'
+        },
+        {
+            text: 'Exchanges',
+            route: ['exchanges'],
+            icon: 'exchange'
+        },
+        {
+            text: 'Settings',
+            route: ['settings'],
+            icon: 'cog'
+        },
+        {
+            text: 'Help',
+            route: ['help'],
+            icon: 'help'
+        },
+        {
+            text: 'About',
+            route: ['about'],
+            icon: 'about',
+            menu: [
+            {
+                text: 'Contact Us',
+                route: ['about', 'contact'],
+                icon: 'contact',
+            }]
+        }
+    ];
+
+    return new MenuItems;
+
+});
---
+++ resources/public/js/models/menuItem.js
@@ -0,0 +1,9 @@
+define(['backbone'], function(Backbone) {
+
+    var MenuItem = Backbone.Model.extend({
+
+    });
+
+    return MenuItem;
+
+});
