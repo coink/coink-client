c1d9dc6e7fcb65443676c39cfaecb5ebd0b7576b
Roey Chasman <rchasman@email.arizona.edu>
2014-03-31 23:21:14 -0700

    added some stuff to landing so it's not a blank page.

--- resources/public/js/components/landing.js
+++ resources/public/js/components/landing.js
@@ -4,7 +4,15 @@ define(['react', 'jquery'], function(React, $) {
         displayName: 'Landing',
 
         render: function() {
-            return React.DOM.div({id: "landing"});
+            return React.DOM.div({id: "landing"},
+                React.DOM.h1({}, "COINK!!!!!!",
+                    React.DOM.br({},
+                        React.DOM.p({},
+                            "CRYPTO CURRENCY HOLDINGS VALUE AGGREGATION MADE SIMPLE"
+                        )
+                    )
+                )
+            );
         }
     });
 
