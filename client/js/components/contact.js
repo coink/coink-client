b29206b7f02693d3f7aaa681cde8c1126f4ed295
Chris LaRose <cjlarose@email.arizona.edu>
2014-05-12 18:30:57 +0000

    Add emails

--- client/js/components/contact.js
+++ client/js/components/contact.js
@@ -5,10 +5,22 @@ function(React){
         displayName: 'Contact',
 
         render: function() {
+            var emails = [
+                "rchasman@coink.io",
+                "chris@coink.io",
+                "matt@coink.io",
+                "soumya92@coink.io",
+                "ashley@coink.io",
+                "zach@coink.io",
+                "shane@coink.io",
+                "kate@coink.io"
+            ];
             return React.DOM.div({},
                 React.DOM.h1({}, "Contact Us"),
-                React.DOM.p({},
-                    "rchasman@coink.io chris@coink.io")
+                React.DOM.ul({},
+                    _.map(emails, function(e) {
+                        return React.DOM.li({}, e);
+                    })));
             );
         }
     });
