53ee5bcc5e82ae61ee863af60ac0dd4cd142a597
Roey Chasman <rchasman@email.arizona.edu>
2014-05-11 19:00:33 -0700

    Removed unnessecary models.

--- client/js/collections/allDatas.js
+++
@@ -1,8 +0,0 @@
-define(['backbone', 'models/allData'], function(Backbone, AllData) {
-
-    var AllDatas = Backbone.Collection.extend({
-        model: AllData,
-    });
-
-    return AllDatas;
-});
